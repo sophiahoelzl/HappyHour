b12d4ca8b7b5f81f017467ccab2d8eecbb6fea85
saraHoeferer <sara.hoeferer@stud.fh-campuswien.ac.at>
2022-05-24 19:43:54 +0200

    Änderungen Sara (Impressum, Startseite (inkl. Css), Termine, Profil, Bars und cocktails eingefügt)

--- files/js-files/bars.js
+++ files/js-files/bars.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
             return response.json();
         })
         .then(bars => {
+            let cnt = 0;
+            let cnt2 = 0;
             for (let bar of Array.from(bars)) {
                 /*
                 <div class="row">
@@ -31,24 +33,54 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     </div>
                 </div>
                 */
-
                 let div = document.createElement('div');
-                document.querySelector('main').append(div);
-                div.class = 'row';
+                if (cnt == 0 || cnt % 3 == 0){
+                    document.querySelector('main').appendChild(div);
+                }
+                div.className = 'row rowClass';
                 
+                let divs = document.getElementsByClassName("rowClass");
                 let div2 = document.createElement('div');
-                div.append(div2);
-                div2.class = 'col-sm';
+                //div.appendChild(div2);
+                if (cnt < 3){
+                    divs[0].appendChild(div2);
+                } else if (cnt < 6){
+                    divs[1].appendChild(div2);
+                } else if (cnt < 9){
+                    divs[2].appendChild(div2);
+                } else if (cnt < 12){
+                    divs[3].appendChild(div2);
+                } else if (cnt < 15){
+                    divs[4].appendChild(div2);
+                } else if (cnt < 18){
+                    divs[5].appendChild(div2);
+                } else if (cnt < 21){
+                    divs[6].appendChild(div2);
+                } else {
+                    divs[0].appendChild(div2);
+                }
+                div2.className = 'col-4 text-center';
 
                 let article = document.createElement('article');
                 div2.append(article);
+                article.className = "divsBars"
 
                 let h2 = document.createElement('h3');
                 article.append(h2);
                 h2.innerHTML = bar[1].name;
+                h2.className = "h2Article";
+
+                let div3 = document.createElement("div");
+                div3.className = "row";
+                article.append(div3);
+
+                let div4 = document.createElement("div");
+                div4.className = "col";
+                div3.append(div4);
 
                 let img = document.createElement('img');
-                article.append(img);
+                div4.append(img)
+                //article.append(img);
                 //replaced this
                 //img.src = bar[1].img_url;
                 //with this
@@ -57,12 +89,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 img.alt = 'alt';
                 img.width = '300';
                 img.height = '200';
+                img.className = "imgArticle";
+                
+                let div5 = document.createElement("div");
+                div5.className = "col text-left";
+                div3.append(div5);
+                
 
                 let p = document.createElement('p');
-                article.append(p);
-                p.innerHTML = 'rating (4.8/5 stars), rating count, address, more than 1 photo?, place holder photo?';
-
-                document.querySelector('main').appendChild(div);
+                div5.append(p);
+                //article.append(p);
+                //p.innerHTML = 'rating (4.8/5 stars), rating count, address, more than 1 photo?, place holder photo?';
+                p.innerHTML = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+                p.className = "pArticle";
+                cnt = cnt +1;
             }
         }).catch(err => console.error(`Fetch problem: ${err.message}`));
 });
--- files/js-files/cocktails.js
+++ files/js-files/cocktails.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     fetch('/api/cocktails')
         .then(response => response.json())
         .then(cocktails => {
+            let cnt = 0;
             for (let cocktail of Array.from(cocktails)) {
             /*    <div class="row">
             <div class="col-sm">
@@ -13,34 +14,82 @@ document.addEventListener("DOMContentLoaded", function (event) {
             </div>
         </div>*/
                 let div = document.createElement('div');
-                document.querySelector('main').append(div);
-                div.class = 'row';
+                if (cnt == 0 || cnt % 4 == 0){
+                    document.querySelector('main').append(div);
+                }
+                div.className = 'row rowClass';
 
+                let divs = document.getElementsByClassName("rowClass");
                 let div2 = document.createElement('div');
-                div.append(div2);
-                div2.class = 'col-sm';
+
+                if (cnt < 4){
+                    divs[0].append(div2);
+                } else if (cnt < 8){
+                    divs[1].append(div2);
+                } else if (cnt < 12){
+                    divs[2].append(div2);
+                } else if (cnt < 16){
+                    divs[3].append(div2);
+                } else if (cnt < 20){
+                    divs[4].append(div2);
+                } else if (cnt < 24){
+                    divs[5].append(div2);
+                } else if (cnt < 28){
+                    divs[6].append(div2);
+                } else {
+                    divs[0].append(div2);
+                }
+                //div.append(div2);
+                div2.className = 'col text-center';
+
+                let article = document.createElement("article");
+                div2.append(article);
+                article.className = "divsCocktails";
 
                 let h2 = document.createElement('h2');
-                div2.append(h2);
+                article.append(h2);
                 h2.innerHTML = cocktail[1].strDrink;
+                h2.className = "h2Cocktails";
+                
+                let div3 = document.createElement("div");
+                div3.className="row";
+                article.append(div3);
+
+                let div4 = document.createElement("div");
+                div3.append(div4);
+                div4.className = "col"
 
                 let img = document.createElement('img');
-                div2.append(img)
+                div4.append(img)
                 img.src = cocktail[1].strDrinkThumb;
                 img.alt = 'alt';
                 img.width = '200';
                 img.height = '200';
+                img.className = "imgCocktails";
+
+                let div5 = document.createElement("div");
+                div5.className = "row";
+                article.append(div5);
+
+                let div6 = document.createElement("div");
+                div6.className = "col text-right";
+                div5.append(div6);
 
                 let a = document.createElement('a');
-                div2.append(a);
+                div6.append(a);
                 a.innerHTML = 'Recipe';
 
+                let div7 = document.createElement("div");
+                div5.append(div7);
+                div7.className = "col text-left";
+
                 let input = document.createElement('input');
-                div2.append(input);
+                div7.append(input);
                 input.type = 'button';
                 input.value = 'Like!';
 
-                document.querySelector('main').appendChild(div);
+                //document.querySelector('main').appendChild(div);
+                cnt = cnt +1;
             }
         });
 });
